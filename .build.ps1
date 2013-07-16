6d563f7e24f7c9035b6bc629a916cd06a807baf5
Slav Ivanyuk <slav.isv@gmail.com>
2013-07-16 17:36:30 -0400

    updated nuget version

--- .build.ps1
+++ .build.ps1
@@ -98,7 +98,7 @@ task NuGet Package, Version, {
 <package>
 	<metadata>
 		<id>TeapplixAccess</id>
-		<version>$Version-alpha3</version>
+		<version>$Version-alpha4</version>
 		<authors>Slav Ivanyuk</authors>
 		<owners>Slav Ivanyuk</owners>
 		<projectUrl>https://github.com/slav/TeapplixAccess</projectUrl>
