bf97da9e76b825756d939cb1ce3ea2e9f4220a5b
Dmitry Sobornykov <d1984v@gmail.com>
2014-05-07 15:19:54 +0300

    added checks to prevent nullreference exceptions, tiny refactoring

--- src/TeapplixAccess/Services/TeapplixExportFileParser.cs
+++ src/TeapplixAccess/Services/TeapplixExportFileParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using LINQtoCSV;
 using TeapplixAccess.Models.File;
 
@@ -16,62 +17,67 @@ namespace TeapplixAccess.Services
 
 			using( var reader = new StreamReader( source ) )
 			{
-				foreach( var row in cc.Read< TeapplixRawDataRow >( reader, new CsvFileDescription { FirstLineHasColumnNames = true } ) )
-				{
-					var order = new TeapplixOrder();
-
-					order.OrderSource = row[ 0 ].Value;
-					order.AccountId = row[ 1 ].Value;
-					order.TnxId = row[ 2 ].Value;
-					order.TnxId2 = row[ 3 ].Value;
-					order.Date = DateTime.Parse( row[ 4 ].Value, CultureInfo.InvariantCulture );
-					order.PaymentType = row[ 5 ].Value;
-					order.PaymentAuthInfo = row[ 6 ].Value;
-					order.FirstName = row[ 7 ].Value;
-					order.LastName = row[ 8 ].Value;
-					order.Email = row[ 9 ].Value;
-					order.Phone = row[ 10 ].Value;
-					order.Country = row[ 11 ].Value;
-					order.State = row[ 12 ].Value;
-					order.AddressZip = row[ 13 ].Value;
-					order.City = row[ 14 ].Value;
-					order.Address1 = row[ 15 ].Value;
-					order.Address2 = row[ 16 ].Value;
-					order.Tax = row[ 19 ].Value;
-					order.Discount = row[ 20 ].Value;
-					order.Fee = row[ 21 ].Value;
-					order.Carrier = row[ 23 ].Value;
-					order.Class = row[ 24 ].Value;
-					order.Tracking = row[ 25 ].Value;
-					order.Postage = row[ 26 ].Value;
-					order.ItemsCount = int.Parse( row[ 27 ].Value );
-
-					decimal total;
-					if( !decimal.TryParse( row[ 17 ].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out total ) )
-						order.Total = 0m;
-					decimal shipping;
-					if( !decimal.TryParse( row[ 18 ].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out shipping ) )
-						order.Shipping = 0m;
-
-					DateTime shipDate;
-					if( DateTime.TryParse( row[ 22 ].Value, out shipDate ) )
-						order.ShipDate = shipDate;
-
-					var items = new List< TeapplixItem >();
-					for( var i = 0; i < order.ItemsCount; i++ )
-					{
-						var item = this.LoadItem( row, i );
-						items.Add( item );
-					}
-
-					order.Total = total;
-					order.Shipping = shipping;
-					order.Items = items;
-					orders.Add( order );
-				}
+				orders.AddRange( cc.Read< TeapplixRawDataRow >( reader, new CsvFileDescription { FirstLineHasColumnNames = true } ).Select( this.FillOrder ) );
 
 				return orders;
 			}
+		}
+
+		private TeapplixOrder FillOrder( TeapplixRawDataRow row )
+		{
+			var order = new TeapplixOrder
+			{
+				OrderSource = row[ 0 ].Value ?? string.Empty,
+				AccountId = row[ 1 ].Value ?? string.Empty,
+				TnxId = row[ 2 ].Value ?? string.Empty,
+				TnxId2 = row[ 3 ].Value ?? string.Empty,
+				Date = string.IsNullOrEmpty( row[ 4 ].Value ) ? DateTime.MinValue : DateTime.Parse( row[ 4 ].Value, CultureInfo.InvariantCulture ),
+				PaymentType = row[ 5 ].Value ?? string.Empty,
+				PaymentAuthInfo = row[ 6 ].Value ?? string.Empty,
+				FirstName = row[ 7 ].Value ?? string.Empty,
+				LastName = row[ 8 ].Value ?? string.Empty,
+				Email = row[ 9 ].Value ?? string.Empty,
+				Phone = row[ 10 ].Value ?? string.Empty,
+				Country = row[ 11 ].Value ?? string.Empty,
+				State = row[ 12 ].Value ?? string.Empty,
+				AddressZip = row[ 13 ].Value ?? string.Empty,
+				City = row[ 14 ].Value ?? string.Empty,
+				Address1 = row[ 15 ].Value ?? string.Empty,
+				Address2 = row[ 16 ].Value ?? string.Empty,
+				Tax = row[ 19 ].Value ?? string.Empty,
+				Discount = row[ 20 ].Value ?? string.Empty,
+				Fee = row[ 21 ].Value ?? string.Empty,
+				Carrier = row[ 23 ].Value ?? string.Empty,
+				Class = row[ 24 ].Value ?? string.Empty,
+				Tracking = row[ 25 ].Value ?? string.Empty,
+				Postage = row[ 26 ].Value ?? string.Empty,
+				ItemsCount = string.IsNullOrEmpty( row[ 27 ].Value ) ? 0 : int.Parse( row[ 27 ].Value )
+			};
+
+			decimal total;
+			if( !decimal.TryParse( row[ 17 ].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out total ) )
+				order.Total = 0m;
+
+			decimal shipping;
+			if( !decimal.TryParse( row[ 18 ].Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out shipping ) )
+				order.Shipping = 0m;
+
+			DateTime shipDate;
+			if( DateTime.TryParse( row[ 22 ].Value, out shipDate ) )
+				order.ShipDate = shipDate;
+
+			var items = new List< TeapplixItem >();
+			for( var i = 0; i < order.ItemsCount; i++ )
+			{
+				var item = this.LoadItem( row, i );
+				items.Add( item );
+			}
+
+			order.Total = total;
+			order.Shipping = shipping;
+			order.Items = items;
+
+			return order;
 		}
 
 		private TeapplixItem LoadItem( TeapplixRawDataRow row, int itemNumber )
@@ -79,16 +85,16 @@ namespace TeapplixAccess.Services
 			var startColumn = 28 + 4 * itemNumber;
 
 			var item = new TeapplixItem
-				{
-					Descrption = GetItemDescription( row, startColumn ),
-					Quantity = this.GetItemQuantity( row, startColumn ),
-					Sku = this.GetItemSku( row, startColumn ),
-					Subtotal = this.GetItemSubtotal( row, startColumn )
-				};
+			{
+				Descrption = this.GetItemDescription( row, startColumn ),
+				Quantity = this.GetItemQuantity( row, startColumn ),
+				Sku = this.GetItemSku( row, startColumn ),
+				Subtotal = this.GetItemSubtotal( row, startColumn )
+			};
 			return item;
 		}
 
-		private static string GetItemDescription( TeapplixRawDataRow row, int startColumn )
+		private string GetItemDescription( TeapplixRawDataRow row, int startColumn )
 		{
 			var description = string.Empty;
 			if( row.Count > startColumn )
